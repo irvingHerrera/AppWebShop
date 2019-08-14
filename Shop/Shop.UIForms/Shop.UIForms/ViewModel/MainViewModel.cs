05ffbbf1c38938aafda8cf591c09bec1c5fcae62
Irving Herrera <irving.herreramolina@gmail.com>
2019-08-14 16:02:13 -0500

    page login xamarin forms, login view model

--- Shop/Shop.UIForms/Shop.UIForms/ViewModel/MainViewModel.cs
+++ Shop/Shop.UIForms/Shop.UIForms/ViewModel/MainViewModel.cs
@@ -2,5 +2,10 @@
 {
     public class MainViewModel
     {
+        public LoginViewModel Login { get; set; }
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
     }
 }

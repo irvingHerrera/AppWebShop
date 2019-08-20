bf7a744e0895eb14da8950f5b49b9dce68214c19
Irving Herrera <irving.herreramolina@gmail.com>
2019-08-20 12:01:19 -0500

    funcionalidad login test, add command button login

--- Shop/Shop.UIForms/Shop.UIForms/ViewModel/LoginViewModel.cs
+++ Shop/Shop.UIForms/Shop.UIForms/ViewModel/LoginViewModel.cs
@@ -1,8 +1,43 @@
-﻿namespace Shop.UIForms.ViewModel
+﻿using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Shop.UIForms.ViewModel
 {
     public class LoginViewModel
     {
+        public LoginViewModel()
+        {
+            this.Email = "irving.herreramolina@gmail.com";
+            this.Password = "123456";
+        }
+
         public string Email { get; set; }
         public string Password { get; set; }
+
+        public ICommand LoginCommand => new RelayCommand(Login);
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "You must enter an email", "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "You must enter an password", "Accept");
+                return;
+            }
+
+            if (!this.Email.Equals("irving.herreramolina@gmail.com") || !this.Password.Equals("123456"))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "User or password wrong", "Accept");
+                return;
+            }
+
+            await Application.Current.MainPage.DisplayAlert("Ok", "Fuck yeah!!", "Accept");
+        }
     }
 }
